3915ed0032ce9c506707b3392c4f84d1a45871e8
Daniel Lee <dlee3@swarthmore.edu>
2018-03-26 17:22:31 -0400

    read .csv files into a list

--- stockinfo.py
+++ stockinfo.py
@@ -1,13 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
 import urllib3
+import csv
 
 def main():
+
+    """https://finance.yahoo.com/quote/AAPL/key-statistics?p=AAPL
     url = "https://finance.yahoo.com/quote/GOOG/key-statistics?p=GOOG"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
 
-    print(soup.prettify())
+    gu = soup.prettify())
+    """
+
+    ticker_name = getTicker()
+    print (ticker_name)
+
+
+
+def getTicker():
+    with open('companylist.csv') as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        tickers = []
+        names = []
+        ticker_name = []
+        for row in readCSV:
+            ticker = row[0]
+            name = row[1]
+
+            tickers.append(ticker)
+            names.append(name)
+
+        for i in range(len(tickers)):
+            ticker_name.append(tickers[i] + " , " + names[i])
+
+        return ticker_name
+
+
+
 
 
 
